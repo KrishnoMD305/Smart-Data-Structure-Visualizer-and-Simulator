0a6ba9f3122415e7334bb53ede1b7566a4228cc9
KrishnoMD305 <krishnomd60@gmail.com>
2026-03-03 13:54:29 +0600

    .................

--- main.cpp
+++ main.cpp
@@ -2,17 +2,7 @@
 #include "stackengine.hpp"
 
 int main(){
-    MyStack<int> s1;
-    VisuStack v1(s1);
-    v1.visualize();
-    std::cout<<"***************"<<std::endl;
-    s1.push(14);
-    v1.visualize();
-    s1.push(34);
-    s1.push(344);
-    s1.push(12894);
-    std::cout<<"***************"<<std::endl;
-    v1.visualize();
+    
 
     return 0;
 }
