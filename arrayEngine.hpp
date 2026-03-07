0c13fb60d9bed381e08818e60cc96e09aded57d5
KrishnoMD305 <krishnomd60@gmail.com>
2026-03-07 11:39:58 +0600

    insert last

--- arrayEngine.hpp
+++ arrayEngine.hpp
@@ -3,3 +3,19 @@
 #include "colors.hpp"
 
 
+class MyArray{
+private:
+    int MAX_SIZE = 50;
+    int data[50];
+    int size = 0;
+public:
+    void insert_at_last(int val){
+        if(size==0){
+            data[size] = val;
+            size++;
+            return;
+        }
+        data[size] = val;
+        size++;
+    }
+};
