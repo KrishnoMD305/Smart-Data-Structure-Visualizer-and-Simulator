edcb62d4fbface4125e954a997d16b9878cc8ec3
KrishnoMD305 <krishnomd60@gmail.com>
2026-03-08 09:53:22 +0600

    testing successfull

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 int main(){
     MyArray arr;
     
-    arr.insert_at_first(4);
-    arr.insert_at_last(2);
-    arr.insert_at_last(9);
-    arr.insert_at_last(10);
+    arr.insert_at_first(5);
+    arr.insert_at_last(3);
     arr.insert_at_last(1);
+    arr.insert_at_last(6);
+    arr.insert_at_last(9);
 
     VisuArray vv(arr);
     vv.show();
 
     SortArray sa(arr);
     std::cout<<"\n";
-    sa.bubble_visu();
+    sa.insertion_visu();
     
     
 
