45e4bbd25abccdddfd44708ac18cb0505733b149
KrishnoMD305 <krishnomd60@gmail.com>
2026-05-24 11:48:15 +0600

    tested insertion deletion

--- BSTEngine.hpp
+++ BSTEngine.hpp
@@ -5,13 +5,13 @@
 #include<string>
 
 template<typename T>
-class Node {
+class Noda{
 public:
     T data;
-    Node* left;
-    Node* right;
+    Noda<T>* left;
+    Noda<T>* right;
 
-    Node(T val) {
+    Noda(T val){
         data = val;
         left = nullptr;
         right = nullptr;
@@ -20,43 +20,43 @@ public:
 
 template<typename T>
 class BST{
-    Node* root;
+    Noda<T>* root;
 
-    Node* findMin(Node* r) {
+    Noda<T>* findMin(Noda<T>* r) {
         while (r && r->left != nullptr) {
             r = r->left;
         }
         return r;
     }
-    Node* findMax(Node* r) {
+    Noda<T>* findMax(Noda<T>* r) {
         while (r && r->right != nullptr) {
             r = r->right;
         }
         return r;
     }
 
-    Node* in_private(Node* r, T val){
-        if (r == nullptr) {
-            return new Node(val);
+    Noda<T>* in_private(Noda<T>* r, T val){
+        if(r == nullptr){
+            return new Noda<T>(val);
         }
         if (val < r->data) {
-            r->left = insert(r->left, val);
+            r->left = in_private(r->left, val);
         }
         else if (val > r->data) {
-            r->right = insert(r->right, val);
+            r->right = in_private(r->right, val);
         }
         return r;
     }
 
-    Node* remove_pri(Node* r, T key){
+    Noda<T>* remove_pri(Noda<T>* r, T key){
         if(r == nullptr){
             return nullptr;
         }
         if(key < r->data){
-            r->left = remove(r->left, key);
+            r->left = remove_pri(r->left, key);
         }
         else if(key > r->data){
-            r->right = remove(r->right, key);
+            r->right = remove_pri(r->right, key);
         }
 
         else{
@@ -64,16 +64,16 @@ class BST{
                 delete r;
                 return nullptr;
             }else if(r->left == nullptr){
-                Node* temp = r->right;
+                Noda<T>* temp = r->right;
                 delete r;
                 return temp;
             }else if(r->right == nullptr){
-                Node* temp = r->left;
+                Noda<T>* temp = r->left;
                 delete r;
                 return temp;
             }
 
-            Node* temp = findMin(r->right);
+            Noda<T>* temp = findMin(r->right);
             r->data = temp->data;
             r->right = remove_pri(r->right, temp->data);
         }
@@ -81,35 +81,102 @@ class BST{
         return r;
     }
 
-    void inordr_pri(Node* r){
+    void inordr_pri(Noda<T>* r){
         if(r == nullptr){
             return;
         }  
-        inorder(r->left);
-        cout << r->data << " ";
-        inorder(r->right);
+        inordr_pri(r->left);
+        std::cout<<r->data<<" ";
+        inordr_pri(r->right);
     }
-    void preordr_pri(Node* r){
+    void preordr_pri(Noda<T>* r){
         if(r==nullptr){
             return;
         }
-        cout << r->data << " ";
-        preorder(r->left);
-        preorder(r->right);
+        std::cout<<r->data<<" ";
+        preordr_pri(r->left);
+        preordr_pri(r->right);
     }
-    void postordr_pri(Node* r){
+    void postordr_pri(Noda<T>* r){
         if(r==nullptr){
             return;
         }
-        postorder(r->left);
-        postorder(r->right);
-        cout << r->data << " ";
+        postordr_pri(r->left);
+        postordr_pri(r->right);
+        std::cout<<r->data<<" ";
     }
 
 
+    bool src_pri(Noda<T>* r,T key){
+        if(r==nullptr){
+            return false;
+        }
+        if(r->data==key){
+            return true;
+        }else if(key<r->data){
+            return src_pri(r->left,key);
+        }else{
+            return src_pri(r->right, key);
+        }
+    }
 
 
 public:
+    BST(){
+        root = nullptr;
+    }
+    
+    void insert(T val){
+        root = in_private(root,val);
+    }
+    void remove(T key){
+        root = remove_pri(root,key);
+    }
 
+    bool search(T key){
+        return src_pri(root,key);
+    }
+
+    void inorder(){
+        std::cout<<"\n";
+        inordr_pri(root);
+        std::cout<<"\n";
+    }
+    void preorder(){
+        std::cout<<"\n";
+        preordr_pri(root);
+        std::cout<<"\n";
+    }
+    void postorder(){
+        std::cout<<"\n";
+        postordr_pri(root);
+        std::cout<<"\n";
+    }
+
+    void print_tr(Noda<T>* root,std::string ind="",bool isR=true){
+        if(!root){
+            return;
+        }
+        print_tr(root->right, ind + (isR ? "        " : "│       "), true);
+        std::cout<<ind;
+        if(isR){
+            std::cout<<"┌───────";
+        }else{
+            std::cout<<"└───────";
+        }
+        std::cout<<"["<<root->data<<"]"<<"\n";
+
+        print_tr(root->left, ind + (isR ? "│       " : "        "), false);
+    }
+    void visualize(){
+        if(!root){
+            std::cout<<"Tree is empty\n\n";
+            return;
+        }
+        print_tr(root->right,"        ",true);
+        std::cout<<"        ["<<root->data<<"]\n";
+        print_tr(root->left,"        ",false);
+        std::cout<<"\n\n";
+    }
 
 };
--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 #include "arrayEngine.hpp"
 #include "LinkListEngine.hpp"
 #include "QueueEngine.hpp"
+#include "BSTEngine.hpp"
 
 int main(){
-    
-    
+    BST<int> b;
+    b.visualize();
+    b.insert(6);
+    b.insert(10);
+    b.insert(5);
+    b.visualize();
+    b.remove(45);
+    b.visualize();
+    b.remove(6);
+    b.visualize();
     return 0;
 }
